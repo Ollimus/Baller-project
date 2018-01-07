3edfe764a03a2b8da0b14d564062760457942bcf
Unknown <1500641@edu.karelia.fi>
2018-01-07 02:13:24 +0200

    Groundwork for Audio (script, manager, music)
    Groundwork for Audio (script, manager, music)

---
+++ BallGame/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+    //public AudioClip music;
+
+    // Use this for initialization
+    void Start ()
+    {
+        musicSource.Play();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
