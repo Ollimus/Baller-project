cc9464a871a5517826873b58c62264835db7d90f
Unknown <1500641@edu.karelia.fi>
2018-05-09 02:29:41 +0300

    Input Editor Window, TouchControl change
    Input editor window located in Unity Editor that allows switching from Keyboard input to Mobile one for easy testing. TouchController changed to be static to be more easily handled.

---
+++ BallGame/Assets/_Scripts/EditorInputControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class EditorInputControls : EditorWindow
+{    
+    [MenuItem("Input/Keyboard")]
+    public static void KeyboardControls()
+    {
+        if (TouchControlBehavior.TouchInstance == null) return;
+
+        TouchControlBehavior.TouchInstance.SetActive(false);
+    }
+
+    [MenuItem("Input/Mobile Controls")]    
+    public static void MobileControls()
+    {
+        if (TouchControlBehavior.TouchInstance == null) return;
+
+        TouchControlBehavior.TouchInstance.SetActive(true);
+    }
+}
--- BallGame/Assets/_Scripts/Touch Control/TouchControlBehavior.cs
+++ BallGame/Assets/_Scripts/Touch Control/TouchControlBehavior.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 //Will be used in the future.
 public class TouchControlBehavior : MonoBehaviour
 {
+    public static GameObject TouchInstance;
+
     private void Start()
     {
+        if (TouchInstance == null)
+            TouchInstance = gameObject;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         string OS = SystemInfo.operatingSystem;
 
         if (SceneManager.GetActiveScene().name == "00_MainMenu" || OS.Contains("Windows") || OS.Contains("Linux") || OS.Contains("Mac"))
